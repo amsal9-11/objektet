1fffe70efe5df275d25e075fb9d352dd70b933dc
amsal9-11 <amsal.kalludra@gmail.com>
2024-07-16 12:00:06 +0200

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,9 @@
+produktet = {
+    "tv" : 300,
+    "mause" : 5,
+    "laptop" : 1000,
+    "keyboard" : 50
+}
+print(sorted(produktet.values()))
+x = min(produktet.values())
+print(f"produkti me qmimin me te vogel eshte{x}")
